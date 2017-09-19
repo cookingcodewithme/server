ff8220de564c03073b7b3de265b6ce37e2fb2ee8
Sergei Golubchik <serg@mariadb.org>
2017-09-19 11:18:45 +0200

    MDEV-12672 Replicated TIMESTAMP fields given wrong value near DST change
    Implement a special Copy_field method for timestamps, that copies
    timestamps without converting them to MYSQL_TIME (the conversion
    is lossy around DST change dates).

--- sql/field_conv.cc
+++ sql/field_conv.cc
@@ -417,6 +417,18 @@ static void do_field_decimal(Copy_field *copy)
 }
 
 
+static void do_field_timestamp(Copy_field *copy)
+{
+  DBUG_ASSERT(copy->from_field->type() == MYSQL_TYPE_TIMESTAMP);
+  DBUG_ASSERT(copy->to_field->type() == MYSQL_TYPE_TIMESTAMP);
+  ulong sec_part;
+  Field_timestamp *from= static_cast<Field_timestamp*>(copy->from_field);
+  Field_timestamp *to= static_cast<Field_timestamp*>(copy->to_field);
+  my_time_t ts= from->get_timestamp(&sec_part);
+  to->store_TIME(ts, sec_part);
+}
+
+
 static void do_field_temporal(Copy_field *copy)
 {
   MYSQL_TIME ltime;
@@ -724,7 +736,9 @@ Copy_field::get_copy_func(Field *to,Field *from)
           ((to->table->in_use->variables.sql_mode &
             (MODE_NO_ZERO_IN_DATE | MODE_NO_ZERO_DATE)) &&
              mysql_type_to_time_type(to->type()) != MYSQL_TIMESTAMP_TIME))
-        return do_field_temporal;
+        return (from->type() == MYSQL_TYPE_TIMESTAMP &&
+                to->type() == MYSQL_TYPE_TIMESTAMP)
+                ? do_field_timestamp : do_field_temporal;
       /* Do binary copy */
     }
     // Check if identical fields
